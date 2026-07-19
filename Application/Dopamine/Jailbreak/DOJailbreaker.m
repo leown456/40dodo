ae902deabacf2a3bddd2c5809cf2797b82e453e0
leown123 <136781210+leown123@users.noreply.github.com>
2026-07-19 20:05:54 +0800

    Update DOJailbreaker.m

--- Application/Dopamine/Jailbreak/DOJailbreaker.m
+++ Application/Dopamine/Jailbreak/DOJailbreaker.m
@@ -702,8 +702,11 @@ int gethuizhipid()
 	NSLog(@"小罪ADD: do提权程序 获取DeltaForceClient proc：0x%llx 成功!",proc);
 	//proc_csflags_set(proc, CS_PLATFORM_BINARY);
 	// Allow invalid pages
-	cs_allow_invalid(proc, true);
-	//cs_allow_invalid(proc, 0);
+	//cs_allow_invalid(proc, true);
+	proc_csflags_set(proc, 0x10000004);
+    proc_csflags_clear(proc, 0x300);
+	cs_allow_invalid(proc, 0);
+	proc_rele(proc);
 
 	NSLog(@"小罪ADD: do提权程序 DeltaForceClient 提权成功! ");
 
@@ -786,7 +789,7 @@ int gethuizhipid()
 
 	proc_csflags_set(targetproc, CS_PLATFORM_BINARY);
 
-	proc_rele(proc);
+	
 	//proc_rele(targetproc);
 
 	
